5d921bf4b58dcbb81ca73eb982d609a807583e8a
NgTzeYang <ngty77@gmail.com>
2010-05-03 17:34:32 +0800

    Really minor adjustment to spec, added one more level of description.

--- spec/clamsy/invalid_printer_spec.rb
+++ spec/clamsy/invalid_printer_spec.rb
@@ -4,13 +4,17 @@ class InvalidPrinter < Clamsy::BasePrinter ; end
 
 describe 'Invalid printer' do
 
-  behaves_like 'has standard files support'
+  describe '> converting docs to pdf' do
 
-  it 'should raise Clamsy::ImplementationNotFoundError' do
-    tmp_doc = tmp_file('doc')
-    lambda { InvalidPrinter.docs_to_pdf([tmp_doc], "#{__FILE__}.pdf") }.
-      should.raise(Clamsy::ImplementationNotFoundError).
-      message.should.equal("InvalidPrinter.doc_to_pdf not implemented.")
+    behaves_like 'has standard files support'
+
+    it 'should raise Clamsy::ImplementationNotFoundError' do
+      tmp_doc = tmp_file('doc')
+      lambda { InvalidPrinter.docs_to_pdf([tmp_doc], "#{__FILE__}.pdf") }.
+        should.raise(Clamsy::ImplementationNotFoundError).
+        message.should.equal("InvalidPrinter.doc_to_pdf not implemented.")
+    end
+
   end
 
 end
