03d97d4f20f010bb18dbadbd3249656d9c3dce5a
NgTzeYang <ngty77@gmail.com>
2010-11-10 20:29:53 +0800

    Relocated java artifacts.

--- lib/clamsy/pdf.rb
+++ lib/clamsy/pdf.rb
@@ -2,7 +2,7 @@ module Clamsy
   class PDF
 
     JAVA_CLASS_PATH = %w{iText-5.0.3 utils}.map do |name|
-      File.expand_path("../java/#{name}.jar", __FILE__)
+      File.expand_path("../../java/#{name}.jar", __FILE__)
     end.join(':')
 
     def self.merge(srcs, dest)
